abe29494dd7b71c049cfb564cff998045fc76040
kiri <satoshi_ebihara@xica.net>
2014-08-08 04:02:07 +0900

    add package classifiers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 install_requires = [
-    'djehuty==0.0.2',
+    'djehuty==0.0.3',
 ]
 
 setup(
@@ -12,7 +12,20 @@ setup(
     description='chat bot service for djehuty',
     author='xica development team',
     author_email='info@xica.net',
-    url='http://xica-inc.com',
+    url='https://github.com/xica/djehutyslack',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
     packages=find_packages(),
     install_requires=install_requires,
 )
